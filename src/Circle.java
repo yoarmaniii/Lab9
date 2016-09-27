cb3f654a9b1cc1d397b403963967863b4ff6326c
Armani Raleigh <armanijolee@gmail.com>
2016-09-27 18:43:55 -0400

    All Constructors and Methods are accounted for

--- src/Circle.java
+++ src/Circle.java
@@ -6,21 +6,31 @@ public class Circle {
 	public double area;
 	public double circumference;
 	
+	// Declare your constructor
 	public Circle (double rad){
 		radius = rad;
 	}
+	//Acquire your methods for the area
 	public double getArea(){
 		double area = Math.PI * (radius * radius);
 		return area;
-	}
+	}//Acquire method for the formatted area
 	public String getFormattedArea(){
-		area = getArea();
+		String myArea = formatNumber(getArea());
+		return myArea;
+	}//Acquire method for the formatted number
+	private String formatNumber(double x){
+		String formattedNumber = String.format("%,.2f", x);
+		return formattedNumber;
+
 	}
 	public double getCircumference(){
+		double circumfernce = 2* Math.PI*(radius);
 		return circumference;
 	}
 	public String getFormattedCircumference(){
-		return
+		String myCirc = formatNumber(getCircumference());
+		return myCirc;
 	}
 }	//End of Circle class
 
