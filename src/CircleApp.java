913911ae41e7e8df6b67a44a220894ed7676c5d2
Armani Raleigh <armanijolee@gmail.com>
2016-09-28 11:35:01 -0400

    Working

--- src/CircleApp.java
+++ src/CircleApp.java
@@ -1,20 +1,37 @@
 import java.util.Scanner;
 public class CircleApp {
 
+	//declare variables
 	public static void main(String[] args) {
+		boolean cont = true;
+		String userCont= "y";
 		System.out.println(" Welcome to the Circle Tester!");
+
+		while (cont){
 		System.out.println(" Enter the radius: ");
 		Scanner input = new Scanner(System.in);
-		double userRadius = input.nextDouble();
-		
-		
-		
-		//Userinput for the radius
+		double userRadius = input.nextDouble(); //Get userinput value for radius
+		input.nextLine();
+		//show output of the circumference and area
 		Circle myCircle = new Circle(userRadius);
 		System.out.println("The circumference is : " + myCircle.getFormattedCircumference());
 		System.out.println("The area is : " + myCircle.getFormattedArea());
 		
+		//make while loop to either continue again or quit
+		System.out.println("Would you like to continue? yes/No");
+		String userinput = input.nextLine();
 		
-	}
+		if (userinput.equals("yes")){
+			cont = true;
+		}
+		
+		else{userinput.equals("no");
+			cont = false;
+		}
+		
+		}//End of while loop
+		System.out.println("Goodbye!");
 
-}
+	}// End of main method
+
+}//End of class
